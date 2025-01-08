4ff9012262b15e997362bdda9a0b1d53e0a96fb2
tpthanh2006 <tranpthanh2006@gmail.com>
2025-01-07 23:45:02 -0600

    git commit -m 'set up imsomnia'

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const app = express();
 
+// Routes
+app.get("", (req, res) => {
+  res.send("Home Page");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
