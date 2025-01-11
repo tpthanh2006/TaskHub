253061ca7e9fb3e23f4643c2ebc32d21f616a158
tpthanh2006 <tranpthanh2006@gmail.com>
2025-01-10 23:14:34 -0600

    initialize frontend structure and update backend task routes

--- backend/routes/taskRoute.js
+++ backend/routes/taskRoute.js
@@ -3,10 +3,10 @@ const Task = require("../models/taskModel");
 const { createTask, getTasks, getTask, deleteTask, updateTask } = require("../controllers/taskController");
 const router = express.Router();
 
-router.post("/api/tasks", createTask);
-router.get("/api/tasks", getTasks);
-router.get("/api/tasks/:id", getTask);
-router.delete("/api/tasks/:id", deleteTask);
-router.put("/api/tasks/:id", updateTask);
+router.post("/", createTask);
+router.get("/", getTasks);
+router.get("/:id", getTask);
+router.delete("/:id", deleteTask);
+router.put("/:id", updateTask);
 
 module.exports = router;
