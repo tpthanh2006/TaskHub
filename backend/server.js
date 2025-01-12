e1efcec6dfeb4fc638cf56d9c23e2e07c6dc0515
tpthanh2006 <tranpthanh2006@gmail.com>
2025-01-12 11:00:26 -0600

    .

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 //Give server.js permission to access MongoDB URI
 const dotenv = require("dotenv").config();
+dotenv.config();
 
 const express = require("express");
+const mongoose = require("mongoose");
 const connectDB = require("./config/connectDB");
 const Task = require("./models/taskModel")
 const taskRoutes = require("./routes/taskRoute");
@@ -14,39 +16,33 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 //Cors need to come on top of routes
-app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "https://taskhub-app-bf9565735ec7.herokuapp.com"],
-}));
+app.use(
+  cors()
+);
 
 app.use("/api/tasks", taskRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html"));
-  })
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running...");
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'));
   });
+} else {
+  // Routes
+  app.get('/', (req, res) => {
+  res.send('Home Page');
+  }); 
 }
 
 const PORT = process.env.PORT || 5000;
-
-const startServer = async () => {
-  try {
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then( async () => {
     await connectDB();
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-}
+  })
+  .catch((err) => console.log(err));
 
 startServer();
