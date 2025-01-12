672d59f09648dd632adf65d15d46d5e3f2691f42
tpthanh2006 <tranpthanh2006@gmail.com>
2025-01-11 20:53:18 -0600

    update environment configuration and server CORS settings; add frontend directory to .gitignore

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "taskhub.onrender.com"],
+    "http://taskhub.onrender.com"],
 }));
 
 app.use("/api/tasks", taskRoutes);
