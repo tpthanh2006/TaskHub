4ed7b8ce24a0174636e529cb2208a1dea1cebb83
tpthanh2006 <tranpthanh2006@gmail.com>
2025-01-12 11:12:36 -0600

    Refactor server.js for improved structure and error handling

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,9 @@
 //Give server.js permission to access MongoDB URI
 const dotenv = require("dotenv").config();
-dotenv.config();
 
 const express = require("express");
-const mongoose = require("mongoose");
 const connectDB = require("./config/connectDB");
+const mongoose = require("mongoose");
 const Task = require("./models/taskModel")
 const taskRoutes = require("./routes/taskRoute");
 const cors = require("cors");
@@ -16,33 +15,39 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 //Cors need to come on top of routes
-app.use(
-  cors()
-);
+app.use(cors());
 
 app.use("/api/tasks", taskRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
-  app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../", "frontend", "build", "index.html"));
   });
 } else {
-  // Routes
-  app.get('/', (req, res) => {
-  res.send('Home Page');
-  }); 
+  app.get("/", (req, res) => {
+    res.send("Home Page");
+  });
 }
 
+// Routes
+app.get("", (req, res) => {
+  res.send("Home Page");
+});
+
 const PORT = process.env.PORT || 5000;
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then( async () => {
+
+const startServer = async () => {
+  try {
     await connectDB();
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
-  })
-  .catch((err) => console.log(err));
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
 
 startServer();
