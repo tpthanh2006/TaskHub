7edd57b05f7fea669ba4aa2d48c6aedd4d985096
tpthanh2006 <tranpthanh2006@gmail.com>
2025-01-12 01:47:21 -0600

    Fix Heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const dotenv = require("dotenv").config();
 
 const express = require("express");
 const connectDB = require("./config/connectDB");
-const mongoose = require("mongoose");
 const Task = require("./models/taskModel")
 const taskRoutes = require("./routes/taskRoute");
 const cors = require("cors");
@@ -18,7 +17,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://taskhub-app-bf9565735ec7.herokuapp.com/"],
+    "https://taskhub-app-bf9565735ec7.herokuapp.com"],
 }));
 
 app.use("/api/tasks", taskRoutes);
@@ -32,14 +31,9 @@ if (process.env.NODE_ENV === "production") {
   })
 } else {
   app.get("/", (req, res) => {
-    res.send("Home Page");
+    res.send("API is running...");
   });
 }
-
-// Routes
-app.get("", (req, res) => {
-  res.send("Home Page");
-});
 
 const PORT = process.env.PORT || 5000;
 
