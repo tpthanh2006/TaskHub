94ca3fda5af682b7ff11c959e2351d457fc465c4
tpthanh2006 <tranpthanh2006@gmail.com>
2025-01-11 20:55:05 -0600

    update CORS settings to allow requests from the new production URL

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "http://taskhub.onrender.com"],
+    "https://taskhub-l93f.onrender.com"],
 }));
 
 app.use("/api/tasks", taskRoutes);
